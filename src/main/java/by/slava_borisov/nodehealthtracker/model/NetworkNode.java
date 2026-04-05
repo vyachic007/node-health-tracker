ae3a9d694c99b84fed92358fc282fa2c4c2287c8
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-05 20:04:21 +0300

    Создал сущность NetworkNode

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/model/NetworkNode.java
@@ -0,0 +1,49 @@
+package by.slava_borisov.nodehealthtracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "network_nodes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class NetworkNode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @Column(name = "host", nullable = false)
+    private String host;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+}
