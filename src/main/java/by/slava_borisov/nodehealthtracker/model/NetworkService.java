c3eb0ce7a0b0be5b0377ab9c84ad6f24e5c03f84
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-10 14:21:28 +0300

    Создал сущность ServiceStatus

--- src/main/java/by/slava_borisov/nodehealthtracker/model/NetworkService.java
+++ src/main/java/by/slava_borisov/nodehealthtracker/model/NetworkService.java
@@ -1,6 +1,13 @@
 package by.slava_borisov.nodehealthtracker.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
