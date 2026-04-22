847d4f5a6133b20e23d89fa4a6e474a85b6a9636
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-22 15:16:20 +0300

    Создал маппер для сетевого узла

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/mapper/NetworkNodeMapper.java
@@ -0,0 +1,33 @@
+package by.slava_borisov.nodehealthtracker.mapper;
+
+import by.slava_borisov.nodehealthtracker.dto.node.NodeCreateRequest;
+import by.slava_borisov.nodehealthtracker.dto.node.NodeResponse;
+import by.slava_borisov.nodehealthtracker.dto.node.NodeUpdateRequest;
+import by.slava_borisov.nodehealthtracker.model.entity.NetworkNode;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface NetworkNodeMapper {
+
+    @Mapping(target = "ownerId", source = "owner.id")
+    NodeResponse toNodeResponse(NetworkNode networkNode);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    NetworkNode toEntity(NodeCreateRequest nodeCreateRequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "owner", ignore = true)
+    void updateEntityFromDto(NodeUpdateRequest nodeUpdateRequest, @MappingTarget NetworkNode networkNode);
+}
