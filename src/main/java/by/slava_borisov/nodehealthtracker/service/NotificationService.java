7095d9619917a22423969efcd42e4d336027464b
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-26 21:28:07 +0300

    Создал интерфейс сервиса уведомлений

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/service/NotificationService.java
@@ -0,0 +1,21 @@
+package by.slava_borisov.nodehealthtracker.service;
+
+import by.slava_borisov.nodehealthtracker.dto.notification.NotificationSettingCreateRequest;
+import by.slava_borisov.nodehealthtracker.dto.notification.NotificationSettingResponse;
+import by.slava_borisov.nodehealthtracker.dto.notification.NotificationSettingUpdateRequest;
+import by.slava_borisov.nodehealthtracker.dto.notification.SentNotificationResponse;
+
+import java.util.List;
+
+public interface NotificationService {
+
+    NotificationSettingResponse createNotificationSetting(NotificationSettingCreateRequest request);
+
+    NotificationSettingResponse updateNotificationSetting(Long settingId, NotificationSettingUpdateRequest request);
+
+    void deleteNotificationSetting(Long settingId);
+
+    List<NotificationSettingResponse> getCurrentUserNotificationSettings();
+
+    List<SentNotificationResponse> getCurrentUserSentNotifications();
+}
