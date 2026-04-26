ea58563885ae3f3e187d9431027adb2f49e6b851
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-26 20:47:59 +0300

    Создал маппер для сетевого сервиса

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/mapper/NetworkServiceMapper.java
@@ -0,0 +1,32 @@
+package by.slava_borisov.nodehealthtracker.mapper;
+
+import by.slava_borisov.nodehealthtracker.dto.service.ServiceCreateRequest;
+import by.slava_borisov.nodehealthtracker.dto.service.ServiceResponse;
+import by.slava_borisov.nodehealthtracker.dto.service.ServiceUpdateRequest;
+import by.slava_borisov.nodehealthtracker.model.entity.NetworkService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+)
+public interface NetworkServiceMapper {
+
+    @Mapping(source = "node.id", target = "nodeId")
+    ServiceResponse toServiceResponse(NetworkService networkService);
+
+    @Mapping(target = "isEnabled", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "node", ignore = true)
+    NetworkService toEntity(ServiceCreateRequest serviceCreateRequest);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "node", ignore = true)
+    void updateEntityFromDto(ServiceUpdateRequest serviceUpdateRequest, @MappingTarget NetworkService networkService);
+}
