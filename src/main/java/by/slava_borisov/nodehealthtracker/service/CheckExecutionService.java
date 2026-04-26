cab67a4baf59ed2ecdfe62ecc69eb8f4eae338a5
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-26 21:27:04 +0300

    Создал интерфейс сервиса выполнения проверок

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/service/CheckExecutionService.java
@@ -0,0 +1,12 @@
+package by.slava_borisov.nodehealthtracker.service;
+
+import by.slava_borisov.nodehealthtracker.dto.check.CheckResultResponse;
+
+import java.util.List;
+
+public interface CheckExecutionService {
+
+    CheckResultResponse runCheck(Long serviceId);
+
+    List<CheckResultResponse> getCheckHistory(Long serviceId);
+}
