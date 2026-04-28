1cf828a29e5dbf6b71756df5745e44ec439ebc3d
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-28 20:35:19 +0300

    Создал исключение для неверных учетных данных

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package by.slava_borisov.nodehealthtracker.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
