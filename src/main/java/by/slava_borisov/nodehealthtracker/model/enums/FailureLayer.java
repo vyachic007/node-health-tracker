c6239df29928a107332f320e6780e4fdd6654dcc
Vyacheslav Borisov <vyachic007@gmail.com>
2026-04-28 20:22:39 +0300

    Добавил enum уровней отказа

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/model/enums/FailureLayer.java
@@ -0,0 +1,13 @@
+package by.slava_borisov.nodehealthtracker.model.enums;
+
+public enum FailureLayer {
+
+    DNS,
+    NETWORK,
+    PORT,
+    APPLICATION,
+    SSL,
+    PERFORMANCE,
+    HEARTBEAT,
+    UNKNOWN
+}
