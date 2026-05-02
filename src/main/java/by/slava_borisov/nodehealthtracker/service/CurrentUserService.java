c2ed1de069aa6093f0099a6934858a72b3df9230
Vyacheslav Borisov <vyachic007@gmail.com>
2026-05-02 22:36:29 +0300

    Добавил сервис получения текущего пользователя

---
+++ src/main/java/by/slava_borisov/nodehealthtracker/service/CurrentUserService.java
@@ -0,0 +1,8 @@
+package by.slava_borisov.nodehealthtracker.service;
+
+import by.slava_borisov.nodehealthtracker.model.entity.User;
+
+public interface CurrentUserService {
+
+    User getCurrentUser();
+}
