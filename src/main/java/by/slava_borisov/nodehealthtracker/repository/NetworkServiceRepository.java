8dd13606693cf7a346164fca48ec597bc78c3ee1
Vyacheslav Borisov <vyachic007@gmail.com>
2026-05-03 00:18:46 +0300

    Добавил поиск сервиса по heartbeat token

--- src/main/java/by/slava_borisov/nodehealthtracker/repository/NetworkServiceRepository.java
+++ src/main/java/by/slava_borisov/nodehealthtracker/repository/NetworkServiceRepository.java
@@ -4,6 +4,7 @@ import by.slava_borisov.nodehealthtracker.model.entity.NetworkService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NetworkServiceRepository extends JpaRepository<NetworkService, Long> {
 
@@ -12,4 +13,6 @@ public interface NetworkServiceRepository extends JpaRepository<NetworkService, 
     List<NetworkService> findAllByNodeIdOrderByCreatedAtDesc(Long nodeId);
 
     List<NetworkService> findAllByNodeOwnerIdOrderByCreatedAtDesc(Long ownerId);
+
+    Optional<NetworkService> findByHeartbeatToken(String heartbeatToken);
 }
