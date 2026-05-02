99476baa9e2395ddf07ed0e0de209dd1bf1c364d
Vyacheslav Borisov <vyachic007@gmail.com>
2026-05-02 15:02:24 +0300

    Добавил поиск истории проверок сервиса в CheckResultRepository

--- src/main/java/by/slava_borisov/nodehealthtracker/repository/CheckResultRepository.java
+++ src/main/java/by/slava_borisov/nodehealthtracker/repository/CheckResultRepository.java
@@ -3,5 +3,9 @@ package by.slava_borisov.nodehealthtracker.repository;
 import by.slava_borisov.nodehealthtracker.model.entity.CheckResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CheckResultRepository extends JpaRepository<CheckResult, Long> {
+
+    List<CheckResult> findAllByServiceIdOrderByCheckedAtDesc(Long serviceId);
 }
